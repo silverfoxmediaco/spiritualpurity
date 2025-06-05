ef38f1c78fc52cf427150cc95460eee350da4334
James-mcewen <james@silverfoxmedia.co>
2025-06-05 13:49:58 -0500

    fixed config

--- spiritualpurity/src/config/api.js
+++ spiritualpurity/src/config/api.js
@@ -3,7 +3,8 @@
 const API_CONFIG = {
     BASE_URL: process.env.NODE_ENV === 'development' 
       ? 'http://localhost:5001' 
-      : '' // Empty string uses same domain in production
+      : 'https://spiritualpurity.onrender.com' 
+
   };
   
   export default API_CONFIG;
