0a3df34dde6d6cbfe39d057c6de0a33a5739d13f
James-mcewen <james@silverfoxmedia.co>
2025-06-07 14:03:38 -0500

    fixed .org from .com

--- spiritualpurity/src/config/api.js
+++ spiritualpurity/src/config/api.js
@@ -3,7 +3,7 @@
 const API_CONFIG = {
     BASE_URL: process.env.NODE_ENV === 'development' 
       ? 'http://localhost:5001' 
-      : 'https://spiritualpurity.com' 
+      : 'https://spiritualpurity.org' 
 
   };
   
